e518e038d7244c6c70f287b5313f811ba7079c9d
Nicholas Hawk <nhawk@revlocal.com>
2022-02-01 14:20:12 -0500

    Refactored the return statement to include the formatting of the productGroups array and immediately reduce it

--- src/cartCalculator.js
+++ src/cartCalculator.js
@@ -1,13 +1,11 @@
 module.exports.computeTotalPrice = function(productsArray) {
-    let productGroups = buildProductGroupsArray(productsArray);
-    
     //if the products in the cart could be rearranged in such a way as to maximize the savings to the customer
     //2 groups of 4 better than a group of 5 and 3?
     //((299 * 5) * .75) + ((299 * 3) * .90) = 1928.55
     //((299 * 4) * .80) + ((299 * 4) * .80) = 1,913.60
 
-    return new Number(productGroups.reduce((total, current) => {
-        return total += ((299 * current.length) * discountPercetage[current.length])
+    return new Number(buildProductGroupsArray(productsArray).reduce((total, current) => {
+        return total += ((299 * current.length) * discountPercentage[current.length])
     }, 0).toFixed(2)).valueOf();
 };
 
@@ -31,7 +29,7 @@ function buildProductGroupsArray(productsArray) {
     return  productGroups;
 }
 
-const discountPercetage = {
+const discountPercentage = {
     1: 1,
     2: .95,
     3: .90,
