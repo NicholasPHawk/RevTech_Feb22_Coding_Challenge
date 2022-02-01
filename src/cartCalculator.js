c724ecd073c22b08d4c9a18944d795256acc9a04
Nicholas Hawk <nhawk@revlocal.com>
2022-02-01 14:02:41 -0500

    Updated README, added more unit tests, completed logic. Still need to refactor

--- src/cartCalculator.js
+++ src/cartCalculator.js
@@ -1,3 +1,40 @@
 module.exports.computeTotalPrice = function(productsArray) {
-    return 'Dummy Return Value';
+    let productGroups = buildProductGroupsArray(productsArray);
+    
+    //if the products in the cart could be rearranged in such a way as to maximize the savings to the customer
+    //2 groups of 4 better than a group of 5 and 3?
+    //((299 * 5) * .75) + ((299 * 3) * .90) = 1928.55
+    //((299 * 4) * .80) + ((299 * 4) * .80) = 1,913.60
+
+    return new Number(productGroups.reduce((total, current) => {
+        return total += ((299 * current.length) * discountPercetage[current.length])
+    }, 0).toFixed(2)).valueOf();
 };
+
+function buildProductGroupsArray(productsArray) {
+    let productGroups = [];
+
+    for (let product of productsArray) {
+        let foundAGroup = false;
+        for (let productGroup of productGroups) {
+            if (!productGroup.includes(product)) {
+                foundAGroup = true;
+                productGroup.push(product);
+                break;
+            }
+        }
+        if (!foundAGroup) {
+            productGroups.push([product]);
+        }
+    }
+
+    return  productGroups;
+}
+
+const discountPercetage = {
+    1: 1,
+    2: .95,
+    3: .90,
+    4: .80,
+    5: .75,
+}
--- src/cartCalculator.test.js
+++ src/cartCalculator.test.js
@@ -10,7 +10,7 @@ describe('Cart Calculator Tests', function() {
     });
     
     it('Has 3 Of The Same Item', function() {
-        expect(computeTotalPrice(['A','A'])).toEqual(897);
+        expect(computeTotalPrice(['A','A', 'A'])).toEqual(897);
     });
 
     it('Has A Set Of 2 Items', function() {
@@ -29,11 +29,19 @@ describe('Cart Calculator Tests', function() {
         expect(computeTotalPrice(['A', 'B', 'C', 'D', 'E'])).toEqual(1121.25);
     });
 
+    it('Has A Set Of 2 Items AND A Set Of 2 Items', function() {
+        expect(computeTotalPrice(['A', 'A', 'B', 'B'])).toEqual(1136.20);
+    });
+    
     it('Has A Set Of 3 Items AND A Set Of 3 Items', function() {
         expect(computeTotalPrice(['A', 'A', 'B', 'B', 'C', 'C'])).toEqual(1614.60);
     });
 
     it('Has A Set Of 5 Items AND A Set Of 3 Items', function() {
         expect(computeTotalPrice(['A', 'A', 'B', 'B', 'C', 'C', 'D', 'E'])).toEqual(1928.55);
+    });
+    
+    it('Has A Set Of 5 Items AND A Set Of 5 Items AND A Set Of 3 Items', function() {
+        expect(computeTotalPrice(['A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'E', 'E'])).toEqual(3049.80);
     });
 });
